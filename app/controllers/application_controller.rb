d1fe82d7ce7bf5b768a38b3105b43bb7f0255b12
CamilaUlmetePais <camilaulmetepais@gmail.com>
2024-07-24 13:45:23 -0300

    supply & user debugging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  # User Role Authentication
+  def authenticate_cashier
+    redirect_to root_path unless current_user.role >= 1
+  end
+
+  def authenticate_manager
+    redirect_to root_path unless current_user.role >= 2
+  end
+
+  def authenticate_supervisor
+    redirect_to root_path unless current_user.role >= 3
+  end
+
+  def authenticate_owner
+    redirect_to root_path unless current_user.role == 4
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-# Roles allow the owner to give more specific permissions to employees with different clearance.
-  enum role: [:employee, :cashier, :supervisor, :manager, :owner]
 end
