5b55f546093f4a081d5245fcc7f12d8549b5271c
CamilaUlmetePais <camilaulmetepais@gmail.com>
2024-08-26 13:27:30 -0300

    style: partial outflows views refactor

--- app/models/outflow_item.rb
+++ app/models/outflow_item.rb
@@ -21,10 +21,10 @@ class OutflowItem < ApplicationRecord
 		self.supply.name + ": " + trim_zeroes(self.quantity).to_s + " " + self.supply.unit.to_s
 	end
 
-  def receipt_list
+  #def receipt_list
   # formats a string for the receipt (outflows/show)
-    self.supply.name + ": " + trim_zeroes(self.quantity).to_s + " " + self.supply.unit + " x $" + trim_zeroes(self.supply.price).to_s + " = "
-  end
+  #  self.supply.name + ": " + trim_zeroes(self.quantity).to_s + " " + self.supply.unit + " x $" + trim_zeroes(self.supply.price).to_s + " = "
+  #end
 
   def restore_stock
   # On delete or edit, restores supply and corresponding linked product stock to previous value.
@@ -36,6 +36,11 @@ class OutflowItem < ApplicationRecord
         Product.find(link.product_id).update_stock(- self.quantity)
       end
     end
+  end
+
+  def show_quantity
+  # cleans up trailing zeroes for the receipt (outflows/show)
+    trim_zeroes(self.quantity)
   end
 
   def subtotal
