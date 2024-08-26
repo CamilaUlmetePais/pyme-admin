31b55c50b850241bf8700943aedbf708826396b5
CamilaUlmetePais <camilaulmetepais@gmail.com>
2024-08-26 13:27:03 -0300

    style: added better assets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def checkbox(boolean)
     # defines an image (checkmark or empty circle) for the view according to a boolean value
     if boolean
-      image_tag("black_check.png", size: "30x30")
+      image_tag("square_check.svg", size: "30x30")
     else
-      image_tag("black_empty.png", size: "30x30")
+      image_tag("square_empty.svg", size: "30x30")
     end
   end
 
