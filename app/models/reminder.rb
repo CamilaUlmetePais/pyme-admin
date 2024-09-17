02118d41e614a3a7b723460f07e50b8ec6a2756e
CamilaUlmetePais <camilaulmetepais@gmail.com>
2024-09-17 10:29:57 -0300

    fix: custom validation syntax

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,11 +1,11 @@
 class Reminder < ApplicationRecord
 	validates :title, :due_date, presence: true
-  validate :due_date_cannot_be_in_the_past, on: create
+  validate :due_date_cannot_be_in_the_past, on: :create
 
   scope :last_60_days, -> { where('created_at >= ?', 60.days.ago) }
 
   def due_date_cannot_be_in_the_past
     errors.add(:due_date, I18n.t('activerecord.errors.messages.due_date')) if
-      !due_date.blank? and due_date < DateTime.now
+      due_date < DateTime.now
   end
 end
