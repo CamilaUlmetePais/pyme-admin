1661f0954a8a0864fe25f67a38241f10e11a52ab
CamilaUlmetePais <camilaulmetepais@gmail.com>
2024-09-25 10:15:30 -0300

    supply_product_link doc

--- app/models/supply_product_link.rb
+++ app/models/supply_product_link.rb
@@ -1,5 +1,10 @@
+# The products that are sold without being modified in any way can be connected so that the product's stock is updated automatically when the supply is bought, without the user having to update it by hand. 
+# 
+# @modelAttribute product_id [Integer] The ID of the product that will be sold without modification.
+# @modelAttribute supply_id [Integer] The ID of the supply that will be connected to the product. 
+# @validation product_id must be present and unique.
+# @validation supply_id must be present and unique.
 class SupplyProductLink < ApplicationRecord
 	validates :product_id, :supply_id, presence: true
-	validates :product_id, uniqueness: true
-	validates :supply_id, uniqueness: true
+	validates :product_id, :supply_id, uniqueness: true
 end
